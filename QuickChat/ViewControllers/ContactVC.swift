bf7f9d3294b4a6228200afa6333a1defbffa57b4
Truong Mai <truongmv@outlook.com>
2017-05-24 18:02:19 +0700

    done chat new message to a contact member

--- QuickChat/ViewControllers/ContactVC.swift
+++ QuickChat/ViewControllers/ContactVC.swift
@@ -12,7 +12,8 @@ import Firebase
 class ContactVC: UITableViewController {
     
     var items = [User]()
-
+    var selectedUser: User?
+    
     //Downloads users list for Contacts View
     func fetchUsers()  {
         if let id = FIRAuth.auth()?.currentUser?.uid {
@@ -28,85 +29,44 @@ class ContactVC: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.fetchUsers()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.items.count
     }
-
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ContactsCell", for: indexPath) as! ContactsCell
-
-        //let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ContactsCVCell
+        
         cell.profilePic.image = self.items[indexPath.row].profilePic
         cell.nameLabel.text = self.items[indexPath.row].name
         cell.profilePic.layer.borderWidth = 2
-        cell.profilePic.layer.borderColor = GlobalVariables.purple.cgColor
+        cell.profilePic.layer.borderColor = GlobalVariables.blue.cgColor
+        
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if self.items.count > 0 {
+            self.selectedUser = self.items[indexPath.row]
+            self.performSegue(withIdentifier: "ContactToChatSegueID", sender: self)
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ContactToChatSegueID" {
+            let vc = segue.destination as! ChatVC
+            vc.currentUser = self.selectedUser
+        }
     }
-    */
-
 }
