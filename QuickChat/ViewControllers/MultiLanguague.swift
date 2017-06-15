b2a1e8efc75a7cf547844451245b76b4af2aaf65
Truong Mai <truongmv@outlook.com>
2017-06-15 19:45:46 +0700

    Multi

--- QuickChat/ViewControllers/MultiLanguague.swift
+++ QuickChat/ViewControllers/MultiLanguague.swift
@@ -32,6 +32,21 @@ final class MultiLanguague : NSObject {
     var signInBtnStr = Instance.languague == .english ? "Sign In" : "Đăng nhập"
     var createAccBtnStr = Instance.languague == .english ? "Create new account" : "Tạo tài khoản mới"
     
+    //MARK: message
+    var messageItem = Instance.languague == .english ? "Messages" : "Tin nhan"
+    
+    //MARK: Contact
+    var contactItem  = Instance.languague == .english ? "Contacts" : "Danh ba"
+    
+    //MARK: Profile
+    var profileBtnLogout  = Instance.languague == .english ? "Log Out" : "Dang Xuat"
+    var profileItem  = Instance.languague == .english ? "Profile" : "Ho so"
+    
+    //MARK: Setting
+    var settingNotification = Instance.languague == .english ? "Notification" : "Thong bao"
+    var settingTouchIDPasscode = Instance.languague == .english ? "Touch ID & Passcode" : "Van tay va mat khau"
+    var settingLanguague = Instance.languague == .english ? "Language: English" : "Ngon ngu: Tieng Viet"
+    var settingRestoreDefaul = Instance.languague == .english ? "Restore Default Setting" : "Khoi phuc cai dat goc"
     
     //var  = Instance.languague == .english ? "" : ""
 }
